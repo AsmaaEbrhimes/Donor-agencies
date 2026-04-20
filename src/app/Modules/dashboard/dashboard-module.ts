e77946966f9d3b4785e0aa4dca731d8680d6f8df
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-20 13:21:30 +0200

    add new component in module

--- src/app/Modules/dashboard/dashboard-module.ts
+++ src/app/Modules/dashboard/dashboard-module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing-module';
+import { Panel } from './panel/panel';
+import { Menue } from './menue/menue';
+import { MainPageInDashboard } from './Components/main-page-in-dashboard/main-page-in-dashboard';
+import { Projects } from './Components/projects/projects';
+import { SharedModule } from '../../shared/shared-module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Panel,
+    Menue,
+    MainPageInDashboard,
+    Projects
+  ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    SharedModule
   ]
 })
 export class DashboardModule { }
