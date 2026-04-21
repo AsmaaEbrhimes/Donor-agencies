c6565f1fb1c5c68cd66fe187c519543555fa9c5d
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-21 08:56:04 +0200

    add project component

--- src/app/Modules/dashboard/dashboard-module.ts
+++ src/app/Modules/dashboard/dashboard-module.ts
@@ -4,15 +4,15 @@ import { DashboardRoutingModule } from './dashboard-routing-module';
 import { Panel } from './panel/panel';
 import { Menue } from './menue/menue';
 import { MainPageInDashboard } from './Components/Main_dashboard/main-page-in-dashboard/main-page-in-dashboard';
-import { Projects } from './Components/projects/projects';
 import { SharedModule } from '../../shared/shared-module';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { Report } from './Components/Main_dashboard/report/report';
 import { Performanceindicators } from './Components/Main_dashboard/performanceindicators/performanceindicators';
 import { SharedModule as PrimeSharedModule } from 'primeng/api';
 import { MeetingCenter } from './Components/Main_dashboard/meeting-center/meeting-center';
+import { Projects } from './Components/projects/projects';
 @NgModule({
-  declarations: [Panel, Menue, MainPageInDashboard, Projects, Report, Performanceindicators, MeetingCenter],
+  declarations: [Panel, Menue, MainPageInDashboard, Report, Performanceindicators, MeetingCenter, Projects],
   imports: [CommonModule, DashboardRoutingModule, SharedModule, AngularSvgIconModule.forRoot(),PrimeSharedModule],
 })
 export class DashboardModule {}
