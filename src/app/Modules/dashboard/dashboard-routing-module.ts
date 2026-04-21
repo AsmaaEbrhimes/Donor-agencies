4f7a38ee3aa46c721be02b116bd191ca0db56c48
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-21 07:41:33 +0200

    add new route in module

--- src/app/Modules/dashboard/dashboard-routing-module.ts
+++ src/app/Modules/dashboard/dashboard-routing-module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Panel } from './panel/panel';
+import { MainPageInDashboard } from './Components/Main_dashboard/main-page-in-dashboard/main-page-in-dashboard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component:Panel,
+    children: [
+      { path: '', component: MainPageInDashboard}
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
