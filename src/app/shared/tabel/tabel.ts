192aba73dfa137df3fc05339c4b7f9bd480938c0
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-21 07:44:15 +0200

    inhance logic tabel

--- src/app/shared/tabel/tabel.ts
+++ src/app/shared/tabel/tabel.ts
@@ -1,5 +1,14 @@
 import { DatePipe } from '@angular/common';
-import { Component, ContentChild, Input, signal, TemplateRef } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ContentChildren,
+  Input,
+  QueryList,
+  signal,
+  TemplateRef,
+} from '@angular/core';
+import { PrimeTemplate } from 'primeng/api';
 
 @Component({
   selector: 'app-tabel',
@@ -10,7 +19,7 @@ import { Component, ContentChild, Input, signal, TemplateRef } from '@angular/co
 export class Tabel<T extends object> {
   constructor(private datePipe: DatePipe) {}
   tableData: T[] = [];
-  exsistData = signal<string>('');
+  statusData = signal<string>('');
   loadingData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   @Input() bodytabel: any[] = [];
@@ -20,8 +29,8 @@ export class Tabel<T extends object> {
   }
 
   @Input()
-  set no_data(value: string) {
-    this.exsistData.set(value);
+  set status_data(value: string) {
+    this.statusData.set(value);
   }
 
   formatCell(key: string, value: any): string {
@@ -35,20 +44,12 @@ export class Tabel<T extends object> {
     return value;
   }
 
-  @ContentChild(TemplateRef) actionsTpl!: TemplateRef<any>;
 
-  getCellClass(col: any, row: any): string {
-    const value = row[col.value];
-    if (col.value === 'role') {
-      switch (value) {
-        case 'ADMIN':
-          return 'admin-badge';
-        case 'MODERATOR':
-          return 'moderator-badge ';
-        default:
-          return '';
-      }
-    }
-    return '';
+
+  
+  @ContentChildren(PrimeTemplate) templates!: QueryList<PrimeTemplate>;
+  getTemplate(colValue: string): TemplateRef<any> | null {
+    const template = this.templates.find((t) => t.getType() === colValue);
+    return template ? template.template : null;
   }
 }
