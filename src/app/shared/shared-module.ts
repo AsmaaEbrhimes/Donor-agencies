e76d10b169050a89ebcceb9fba88fb75d6aaf4ff
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-21 07:46:06 +0200

    add new property in shared and new componet

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -4,17 +4,24 @@ import { TableModule } from 'primeng/table';
 import { SkeletonModule } from 'primeng/skeleton';
 import { SharedRoutingModule } from './shared-routing-module';
 import { Tabel } from './tabel/tabel';
-
-
+import { Header } from './header/header';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { DrawerModule } from 'primeng/drawer';
+import { SideNav } from './side-nav/side-nav';
+import { DatePipe } from '@angular/common';
+import { NoData } from './no-data/no-data';
 @NgModule({
-  declarations: [
-Tabel
-  ],
+  declarations: [Tabel, Header, SideNav, NoData],
   imports: [
     CommonModule,
     SharedRoutingModule,
     TableModule,
-    SkeletonModule
-  ]
+    SkeletonModule,
+    AngularSvgIconModule.forRoot(),
+    DrawerModule,
+
+  ],
+  exports: [Tabel, Header, SideNav,NoData],
+  providers: [DatePipe],
 })
-export class SharedModule { }
+export class SharedModule {}
