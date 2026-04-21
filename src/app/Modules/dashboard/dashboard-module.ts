5e6da2639c22e116af517cd0241b6902aa7db4ba
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-21 07:41:18 +0200

    add new property in module

--- src/app/Modules/dashboard/dashboard-module.ts
+++ src/app/Modules/dashboard/dashboard-module.ts
@@ -1,25 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DashboardRoutingModule } from './dashboard-routing-module';
 import { Panel } from './panel/panel';
 import { Menue } from './menue/menue';
-import { MainPageInDashboard } from './Components/main-page-in-dashboard/main-page-in-dashboard';
+import { MainPageInDashboard } from './Components/Main_dashboard/main-page-in-dashboard/main-page-in-dashboard';
 import { Projects } from './Components/projects/projects';
 import { SharedModule } from '../../shared/shared-module';
-
-
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { Report } from './Components/Main_dashboard/report/report';
+import { Performanceindicators } from './Components/Main_dashboard/performanceindicators/performanceindicators';
+import { SharedModule as PrimeSharedModule } from 'primeng/api';
+import { MeetingCenter } from './Components/Main_dashboard/meeting-center/meeting-center';
 @NgModule({
-  declarations: [
-    Panel,
-    Menue,
-    MainPageInDashboard,
-    Projects
-  ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule,
-    SharedModule
-  ]
+  declarations: [Panel, Menue, MainPageInDashboard, Projects, Report, Performanceindicators, MeetingCenter],
+  imports: [CommonModule, DashboardRoutingModule, SharedModule, AngularSvgIconModule.forRoot(),PrimeSharedModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
