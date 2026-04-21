a9cd550038f42e8aabcfdcce434aa8bd2cd46f84
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-21 07:44:48 +0200

    add menue is shared

---
+++ src/app/shared/side-nav/side-nav.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav',
+  standalone: false,
+  templateUrl: './side-nav.html',
+  styleUrl: './side-nav.scss',
+})
+export class SideNav implements OnInit {
+  menue = signal<any>([]);
+  ngOnInit(): void {
+    this.DataMenue();
+  }
+
+  DataMenue() {
+    this.menue.set([
+      { name: 'لوحة التحكم', icon: 'assets/Svg/dashboard-square-02.svg', active: true},
+      { name: 'المشاريع', icon: 'assets/Svg/clipboard.svg', active: false},
+      { name: 'المحافظ التمويلية', icon: 'assets/Svg/wallet-01.svg', active: false   },
+      { name: 'التقارير', icon: 'assets/Svg/analytics-01.svg', active: false   },
+      { name: 'إدارة الشركات', icon: 'assets/Svg/plus-sign-square.svg', active: false   },
+      { name: 'الأستشارات', icon: 'assets/Svg/user-group.svg', active: false   },
+      { name: 'طلبات التمويل', icon: 'assets/Svg/plus-sign-square.svg', active: false   },
+    ])
+  }
+}
