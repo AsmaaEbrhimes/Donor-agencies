639647d121e634eee6be90112590605310852988
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-22 08:25:40 +0200

    add finicing component

---
+++ src/app/Modules/dashboard/Components/Main_dashboard/financing/financing.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financing',
+  standalone: false,
+  templateUrl: './financing.html',
+  styleUrl: './financing.scss',
+})
+export class Financing {
+
+}
