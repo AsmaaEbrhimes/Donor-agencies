4196d434967a167b5f80b44c70262df120e47c1a
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-22 08:24:10 +0200

    add new componet

---
+++ src/app/Modules/dashboard/Charts/bar-charts/bar-charts.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar-charts',
+  standalone: false,
+  templateUrl: './bar-charts.html',
+  styleUrl: './bar-charts.scss',
+})
+export class BarCharts {
+
+}
