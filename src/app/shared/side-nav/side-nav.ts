b269ecb53caf1d5b5b26550892ce4448ee966be2
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-22 19:47:56 +0200

    add active route and add route

--- src/app/shared/side-nav/side-nav.ts
+++ src/app/shared/side-nav/side-nav.ts
@@ -14,13 +14,13 @@ export class SideNav implements OnInit {
 
   DataMenue() {
     this.menue.set([
-      { name: 'لوحة التحكم', icon: 'assets/Svg/dashboard-square-02.svg', active: true},
-      { name: 'المحافظ التمويلية', icon: 'assets/Svg/wallet-01.svg', active: false   },
-      { name: 'طلبات التمويل', icon: 'assets/Svg/plus-sign-square.svg', active: false   },
-      { name: 'المشاريع', icon: 'assets/Svg/clipboard.svg', active: false},
-      { name: 'إدارة الشركات', icon: 'assets/Svg/plus-sign-square.svg', active: false   },
-      { name: 'الأستشارات', icon: 'assets/Svg/user-group.svg', active: false   },
-      { name: 'التقارير', icon: 'assets/Svg/analytics-01.svg', active: false   },
-    ])
+      { name: 'لوحة التحكم', icon: 'assets/Svg/dashboard-square-02.svg', active: true, route: '/Dashboard/dashboard' },
+      { name: 'المحافظ التمويلية', icon: 'assets/Svg/wallet-01.svg', active: false, route: '/Dashboard/dashboard/Financial_Governor' },
+      { name: 'طلبات التمويل', icon: 'assets/Svg/plus-sign-square.svg', active: false, route: '' },
+      { name: 'المشاريع', icon: 'assets/Svg/clipboard.svg', active: false, route: '' },
+      { name: 'إدارة الشركات', icon: 'assets/Svg/plus-sign-square.svg', active: false, route: '' },
+      { name: 'الأستشارات', icon: 'assets/Svg/user-group.svg', active: false, route: '' },
+      { name: 'التقارير', icon: 'assets/Svg/analytics-01.svg', active: false, route: '' },
+    ]);
   }
 }
