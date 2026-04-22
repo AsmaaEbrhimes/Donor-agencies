7d6b473b6b1207f2084453f93b2a806613076d9d
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-22 08:27:09 +0200

    add new route component

--- src/app/Modules/dashboard/dashboard-routing-module.ts
+++ src/app/Modules/dashboard/dashboard-routing-module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Panel } from './panel/panel';
 import { MainPageInDashboard } from './Components/Main_dashboard/main-page-in-dashboard/main-page-in-dashboard';
+import { Setting } from './Components/setting/setting';
+import { Meeting } from './Components/meeting/meeting';
 
 const routes: Routes = [
   {
     path: 'dashboard',
-    component:Panel,
+    component: Panel,
     children: [
-      { path: '', component: MainPageInDashboard}
+      { path: '', component: MainPageInDashboard },
+      { path: 'setting', component: Setting },
+      { path: 'meeting', component: Meeting },
     ],
   },
 ];
