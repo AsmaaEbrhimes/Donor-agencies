1dc3cfc45a7bb65b4517c7e36b1fcc7a53bf92d6
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-22 14:15:49 +0200

    ordet menue

--- src/app/shared/side-nav/side-nav.ts
+++ src/app/shared/side-nav/side-nav.ts
@@ -15,12 +15,12 @@ export class SideNav implements OnInit {
   DataMenue() {
     this.menue.set([
       { name: 'لوحة التحكم', icon: 'assets/Svg/dashboard-square-02.svg', active: true},
-      { name: 'المشاريع', icon: 'assets/Svg/clipboard.svg', active: false},
       { name: 'المحافظ التمويلية', icon: 'assets/Svg/wallet-01.svg', active: false   },
-      { name: 'التقارير', icon: 'assets/Svg/analytics-01.svg', active: false   },
+      { name: 'طلبات التمويل', icon: 'assets/Svg/plus-sign-square.svg', active: false   },
+      { name: 'المشاريع', icon: 'assets/Svg/clipboard.svg', active: false},
       { name: 'إدارة الشركات', icon: 'assets/Svg/plus-sign-square.svg', active: false   },
       { name: 'الأستشارات', icon: 'assets/Svg/user-group.svg', active: false   },
-      { name: 'طلبات التمويل', icon: 'assets/Svg/plus-sign-square.svg', active: false   },
+      { name: 'التقارير', icon: 'assets/Svg/analytics-01.svg', active: false   },
     ])
   }
 }
