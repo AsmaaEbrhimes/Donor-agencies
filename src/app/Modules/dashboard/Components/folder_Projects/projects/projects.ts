5f1b0a22b76ea8c84d9ef9299ef71e98dd485cac
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-23 23:46:33 +0200

    create project component

---
+++ src/app/Modules/dashboard/Components/folder_Projects/projects/projects.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  standalone: false,
+  templateUrl: './projects.html',
+  styleUrl: './projects.scss',
+})
+export class Projects implements OnInit {
+  data = signal<any>([]);
+
+  ngOnInit(): void {
+    this.getData();
+    this.bodytabel();
+  }
+
+  getData() {
+    const data = [
+      {
+        project_name: 'السعودية الخضراء',
+        owner_org: 'وزارة البيئة والمياه والزراعة',
+        project_manager: 'نورة الشهري',
+        beneficiaries_count: 50000,
+        status: 'مكتملة', // يمكن استخدامها للتحكم في لون الـ badge
+        approved_funding: 50000,
+        completion_percentage: '100%',
+      },
+      {
+        project_name: 'السعودية الخضراء',
+        owner_org: 'وزارة البيئة والمياه والزراعة',
+        project_manager: 'نورة الشهري',
+        beneficiaries_count: 50000,
+        status: 'جارية',
+        approved_funding: 50000,
+        completion_percentage: '100%',
+      },
+      {
+        project_name: 'السعودية الخضراء',
+        owner_org: 'وزارة البيئة والمياه والزراعة',
+        project_manager: 'نورة الشهري',
+        beneficiaries_count: 50000,
+        status: 'متأخر',
+        approved_funding: 50000,
+        completion_percentage: '100%',
+      },
+      {
+        project_name: 'السعودية الخضراء',
+        owner_org: 'وزارة البيئة والمياه والزراعة',
+        project_manager: 'نورة الشهري',
+        beneficiaries_count: 50000,
+        status: 'جارية',
+        approved_funding: 50000,
+        completion_percentage: '100%',
+      },
+      {
+        project_name: 'السعودية الخضراء',
+        owner_org: 'وزارة البيئة والمياه والزراعة',
+        project_manager: 'نورة الشهري',
+        beneficiaries_count: 50000,
+        status: 'متأخر',
+        approved_funding: 50000,
+        completion_percentage: '100%',
+      }
+      ,
+      {
+        project_name: 'السعودية الخضراء',
+        owner_org: 'وزارة البيئة والمياه والزراعة',
+        project_manager: 'نورة الشهري',
+        beneficiaries_count: 50000,
+        status: 'متأخر',
+        approved_funding: 50000,
+        completion_percentage: '100%',
+      }
+
+    ];
+    this.data.set(data);
+    this.data.set(data);
+  }
+
+  bodytabel() {
+    return [
+      { key: 'المشروع', value: 'project_name' },
+      { key: 'المؤسسة المالكة', value: 'owner_org' },
+      { key: 'مدير المشروع', value: 'project_manager' },
+      { key: 'عدد المستفيدين', value: 'beneficiaries_count' },
+      { key: 'حالة المشروع', value: 'status' },
+      { key: 'التمويل المعتمد', value: 'approved_funding' },
+      { key: 'نسبة الإنجاز', value: 'completion_percentage' },
+    ];
+  }
+}
