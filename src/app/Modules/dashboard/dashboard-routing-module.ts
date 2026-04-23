ea51de461c21c7778738f656daab56faa80a99a5
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-23 11:16:36 +0200

    add new component in routing

--- src/app/Modules/dashboard/dashboard-routing-module.ts
+++ src/app/Modules/dashboard/dashboard-routing-module.ts
@@ -4,6 +4,8 @@ import { Panel } from './panel/panel';
 import { MainPageInDashboard } from './Components/Main_dashboard/main-page-in-dashboard/main-page-in-dashboard';
 import { Setting } from './Components/setting/setting';
 import { Meeting } from './Components/meeting/meeting';
+import { FinancialGovernor } from './Components/financial-governor/financial-governor';
+import { CreateFinancialGovernor } from './Components/create-financial-governor/create-financial-governor';
 
 const routes: Routes = [
   {
@@ -13,6 +15,8 @@ const routes: Routes = [
       { path: '', component: MainPageInDashboard },
       { path: 'setting', component: Setting },
       { path: 'meeting', component: Meeting },
+      { path: 'Financial_Governor', component: FinancialGovernor },
+      { path: 'create_Financial_Governor', component: CreateFinancialGovernor },
     ],
   },
 ];
