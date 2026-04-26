4f6b217a465671955fc9bb17023c3a7064f505f1
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-26 18:42:10 +0300

    feat: add details project component with HTML, SCSS, and unit tests

---
+++ src/app/Modules/dashboard/Components/folder_Projects/details-project/details-project.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-project',
+  standalone: false,
+  templateUrl: './details-project.html',
+  styleUrl: './details-project.scss',
+})
+export class DetailsProject {
+
+}
