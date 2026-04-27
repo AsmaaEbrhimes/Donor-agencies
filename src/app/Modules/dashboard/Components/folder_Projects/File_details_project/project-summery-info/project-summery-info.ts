7e0ed4e025e97cd868ef48667321a3e5a5c38b36
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-27 12:53:10 +0300

    create component project summery

---
+++ src/app/Modules/dashboard/Components/folder_Projects/File_details_project/project-summery-info/project-summery-info.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-summery-info',
+  standalone: false,
+  templateUrl: './project-summery-info.html',
+  styleUrl: './project-summery-info.scss',
+})
+export class ProjectSummeryInfo {
+
+}
