2530e9424b95c686ec7f602b87c0dde5f78835fb
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-27 12:53:30 +0300

    create component project target

---
+++ src/app/Modules/dashboard/Components/folder_Projects/File_details_project/project-target/project-target.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-target',
+  standalone: false,
+  templateUrl: './project-target.html',
+  styleUrl: './project-target.scss',
+})
+export class ProjectTarget {
+
+}
