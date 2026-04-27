483e67144b8c9af50a5d87bb6c5a6b43e019c3fb
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-27 13:54:08 +0300

    add route partnership mangment

--- src/app/shared/side-nav/side-nav.ts
+++ src/app/shared/side-nav/side-nav.ts
@@ -18,7 +18,7 @@ export class SideNav implements OnInit {
       { name: 'المحافظ التمويلية', icon: 'assets/Svg/wallet-01.svg', active: false, route: '/Dashboard/dashboard/Financial_Governor' },
       { name: 'طلبات التمويل', icon: 'assets/Svg/plus-sign-square.svg', active: false, route: '/Dashboard/dashboard/funding_requests' },
       { name: 'المشاريع', icon: 'assets/Svg/clipboard.svg', active: false, route: '/Dashboard/dashboard/projects' },
-      { name: 'إدارة الشركات', icon: 'assets/Svg/plus-sign-square.svg', active: false, route: '' },
+      { name: 'إدارة الشركات', icon: 'assets/Svg/plus-sign-square.svg', active: false, route: '/Dashboard/dashboard/Partnership_Management' },
       { name: 'الأستشارات', icon: 'assets/Svg/user-group.svg', active: false, route: '' },
       { name: 'التقارير', icon: 'assets/Svg/analytics-01.svg', active: false, route: '' },
       { name: 'طلبات الاستشارة', icon: 'assets/Svg/Icon.svg', active: false, route: '' },
