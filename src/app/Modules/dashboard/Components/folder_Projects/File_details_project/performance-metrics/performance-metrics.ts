3582f1f07fa9af4f673c7dded45c52cbf67a425c
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-27 12:52:48 +0300

    create component performance metrics

---
+++ src/app/Modules/dashboard/Components/folder_Projects/File_details_project/performance-metrics/performance-metrics.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-performance-metrics',
+  standalone: false,
+  templateUrl: './performance-metrics.html',
+  styleUrl: './performance-metrics.scss',
+})
+export class PerformanceMetrics {
+
+}
