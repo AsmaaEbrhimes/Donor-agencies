7e1574e9b44f0addc4468c9c0f82fabcc8dd0726
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-27 12:51:29 +0300

    create component descrption project

---
+++ src/app/Modules/dashboard/Components/folder_Projects/File_details_project/descrption-project/descrption-project.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descrption-project',
+  standalone: false,
+  templateUrl: './descrption-project.html',
+  styleUrl: './descrption-project.scss',
+})
+export class DescrptionProject {
+
+}
