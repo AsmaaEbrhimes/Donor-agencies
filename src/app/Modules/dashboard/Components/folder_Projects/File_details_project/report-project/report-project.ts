9fdbbd36baeac2dde10d4ff7b3a5464810f37879
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-27 12:54:12 +0300

    create component report projects

---
+++ src/app/Modules/dashboard/Components/folder_Projects/File_details_project/report-project/report-project.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-project',
+  standalone: false,
+  templateUrl: './report-project.html',
+  styleUrl: './report-project.scss',
+})
+export class ReportProject {
+
+}
