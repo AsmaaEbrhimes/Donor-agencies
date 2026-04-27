4fa2d94146bb57713071306139f722f74ba188b6
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-27 12:51:58 +0300

    create component life cycle project

---
+++ src/app/Modules/dashboard/Components/folder_Projects/File_details_project/life-cycle-project/life-cycle-project.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-life-cycle-project',
+  standalone: false,
+  templateUrl: './life-cycle-project.html',
+  styleUrl: './life-cycle-project.scss',
+})
+export class LifeCycleProject {
+
+}
