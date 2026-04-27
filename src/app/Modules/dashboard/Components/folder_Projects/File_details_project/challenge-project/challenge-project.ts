6bab7745efc90e1261f9810fcb18b7e7ed292e5c
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-27 12:51:09 +0300

    create component challenge project

---
+++ src/app/Modules/dashboard/Components/folder_Projects/File_details_project/challenge-project/challenge-project.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-challenge-project',
+  standalone: false,
+  templateUrl: './challenge-project.html',
+  styleUrl: './challenge-project.scss',
+})
+export class ChallengeProject {
+
+}
