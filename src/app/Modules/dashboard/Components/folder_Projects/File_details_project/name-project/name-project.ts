32383a628bd692c33471de882852298b73423484
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-27 12:52:18 +0300

    create component name project

---
+++ src/app/Modules/dashboard/Components/folder_Projects/File_details_project/name-project/name-project.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-project',
+  standalone: false,
+  templateUrl: './name-project.html',
+  styleUrl: './name-project.scss',
+})
+export class NameProject {
+
+}
