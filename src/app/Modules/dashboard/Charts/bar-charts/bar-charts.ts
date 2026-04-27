98046ade6ad1540dc1c987c9bcd09e60e4f860cc
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-27 23:28:41 +0300

    implement bar chart with dynamic options in BarCharts component

--- src/app/Modules/dashboard/Charts/bar-charts/bar-charts.ts
+++ src/app/Modules/dashboard/Charts/bar-charts/bar-charts.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-bar-charts',
@@ -6,6 +7,46 @@ import { Component } from '@angular/core';
   templateUrl: './bar-charts.html',
   styleUrl: './bar-charts.scss',
 })
-export class BarCharts {
+export class BarCharts implements OnInit {
+  chartOptions: EChartsOption = {};
 
+  ngOnInit(): void {
+    this.chartOptions = {
+      color: ['#1a4b5a'],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: { type: 'shadow' }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        data: ['نشاط 1', 'نشاط 2', 'نشاط 3', 'نشاط 4', 'نشاط 5'],
+        axisLine: { show: true, lineStyle: { color: '#dbe2e8' } },
+        axisTick: { show: false },
+        inverse: true // ليدعم الـ RTL
+      },
+      yAxis: {
+        type: 'value',
+        splitLine: {
+          lineStyle: { type: 'solid', color: '#dbe2e8' }
+        }
+      },
+      series: [
+        {
+          name: 'التقدم',
+          type: 'bar',
+          barWidth: '12px',
+          itemStyle: {
+            borderRadius: [10, 10, 10, 10]
+          },
+          data: [80, 130, 280, 190, 240]
+        }
+      ]
+    };
+  }
 }
