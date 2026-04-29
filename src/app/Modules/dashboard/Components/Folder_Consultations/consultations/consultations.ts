b66080f2f3fff905682b631701bcc6c09f92f033
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-29 09:25:35 +0300

    add Consultations component with HTML and SCSS files

---
+++ src/app/Modules/dashboard/Components/Folder_Consultations/consultations/consultations.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultations',
+  standalone: false,
+  templateUrl: './consultations.html',
+  styleUrl: './consultations.scss',
+})
+export class Consultations {
+
+}
